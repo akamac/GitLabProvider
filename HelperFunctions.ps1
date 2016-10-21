61d37fd39184f64ef4aab05b64895815f56766f4
Alexey Miasoedov <amiasoedov@intermedia.net>
2016-10-21 21:03:16 +0300

    ConvertTo-Hashtable bugfix

--- HelperFunctions.ps1
+++ HelperFunctions.ps1
@@ -57,30 +57,28 @@ function Set-PackageSourcePrivateToken {
 function ConvertTo-Hashtable {
     param(
         [Parameter(Mandatory,ValueFromPipeline)]
-        [array] $Object,
-        [int] $Depth = 3
+        $Object,
+        [int] $Depth = 5
     )
     Process {
-        foreach ($obj in $Object) {
-            if (!$Depth) { return $obj }
-            $ht = [ordered]@{}
-            if ($obj -as [hashtable]) {
-                ($obj -as [hashtable]).GetEnumerator() | % {
-                    if ($_.Value -is [PSCustomObject]) {
-                        $ht.($_.Key) = ConvertTo-Hashtable ($_.Value) ($Depth - 1)
-                    } else {
-                        $ht.($_.Key) = $_.Value
-                    }
+        if (!$Depth) { return $Object }
+        $ht = [ordered]@{}
+        if ($Object -as [hashtable]) {
+            ($Object -as [hashtable]).GetEnumerator() | % {
+                if ($_.Value -is [PSCustomObject]) {
+                    $ht[$_.Key] = ConvertTo-Hashtable ($_.Value) ($Depth - 1)
+                } else {
+                    $ht[$_.Key] = $_.Value
                 }
-                return $ht
-            } elseif ($obj.GetType().Name -eq 'PSCustomObject') {
-                $obj | Get-Member -MemberType Properties | % {
-                    $ht.($_.Name) = ConvertTo-Hashtable (,$obj.($_.Name)) ($Depth - 1)
-                }
-                return $ht
-            } else {
-                return $obj
             }
+            return $ht
+        } elseif ($Object.GetType().Name -eq 'PSCustomObject') {
+            $Object | Get-Member -MemberType Properties | % {
+                $ht[$_.Name] = ConvertTo-Hashtable $Object.($_.Name) ($Depth - 1)
+            }
+            return $ht
+        } else {
+            return $Object
         }
     }
 }
